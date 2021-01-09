cee4569007c618698316891293de7cc734c43d24
JorgeMauricio-USB <jorge.herrera.de@usb.edu.mx>
2021-01-09 12:11:20 -0600

    Initial commit

---
+++ js/apuntes.js
@@ -0,0 +1,86 @@
+// 1. Variables
+// 1.1 var
+var nombre; // variable creada
+console.log (nombre);
+
+nombre="Mauricio";
+console.log (nombre);
+
+var nombre;
+var apellido;
+var edad;
+
+apellido= "Herrera";
+edad= 25;
+
+console.log (nombre);
+console.log (apellido);
+console.log (edad);
+
+var nombre01, 
+    apellido01, 
+    edad01;
+
+nombre01= "José";
+apellido01= "Ambro";
+edad01= 102;
+
+console.log(nombre01,apellido01,edad01);
+
+var nombreProfesor="Arturo";
+console.log(nombreProfesor);
+
+
+
+
+// 1.2 let
+let nombreAlumno= "Andrea";
+nombreAlumno= "Marcos";
+console.log(nombreAlumno);
+
+
+
+
+
+// 1.3 const
+const nombreAlumno01="Daniela";
+
+
+
+// Datos
+// 2.1 Numeros
+let precio= 32;
+console.log(precio);
+
+
+// 2.2 String
+precio= "32";
+console.log(precio);
+
+
+// 2.3  Booleano
+precio= false;
+console.log(precio);
+
+// 3. Operadores
+
+// Introducción a funciones
+console.clear()
+const  numero1 = 20,
+    numero2 = 20,
+    numero3 = "10",
+    numero4 = 10.50,
+    numero5 = -10.50;
+
+let resultado;
+// Suma
+resultado = numero1 + numero2;
+// Resta
+resultado = numero4 - numero5;
+// Multiplicar
+resultado = numero1 * numero2;
+// División
+resultado = numero1 / numero2;
+
+console.log (resultado);
+
